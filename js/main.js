81496921790c4ae56130ca274e4c8817037c233e
ingashevv <ingashevdaudino@gmail.com>
2022-09-17 19:13:20 +0300

    addet transport suppliers

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+    
+    $('.suppliers__slider ul').slick({
+        infinite: false,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 4,
+    });
+
+ 
+
+
+
+});
