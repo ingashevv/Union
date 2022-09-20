df5436808b8861f93a4247170ee3464076155305
ingashevv <ingashevdaudino@gmail.com>
2022-09-20 23:54:03 +0300

    responsive index

--- js/main.js
+++ js/main.js
@@ -1,14 +1,43 @@
 $(function(){
     
+    prevArrow: $('.prev');
+    nextArrow: $('.next');
+
+    prevArrow: $('.prev-arrow');
+    nextArrow: $('.next-arrow');
+
     $('.suppliers__slider ul').slick({
         infinite: false,
         speed: 300,
         slidesToShow: 6,
-        slidesToScroll: 4,
+        slidesToScroll: 1,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+        responsive: [
+            {
+              breakpoint: 360,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+          ],
     });
 
- 
-
-
+    $('.reviews__slider ul').slick({
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.prev-arrow'),
+        nextArrow: $('.next-arrow'),
+        responsive: [
+            {
+              breakpoint: 360,
+              settings: {
+                slidesToShow: 1
+              }
+            },
+          ],
+    });
 
 });
