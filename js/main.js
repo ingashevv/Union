9dfd78b294b6acfcf402e1b9bb24a37fbfa6f248
ingashevv <ingashevdaudino@gmail.com>
2022-09-26 19:57:15 +0300

    corrected order

--- js/main.js
+++ js/main.js
@@ -1,43 +1,56 @@
-$(function(){
-    
-    prevArrow: $('.prev');
-    nextArrow: $('.next');
+$(function () {
 
-    prevArrow: $('.prev-arrow');
-    nextArrow: $('.next-arrow');
 
-    $('.suppliers__slider ul').slick({
-        infinite: false,
-        speed: 300,
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        prevArrow: $('.prev'),
-        nextArrow: $('.next'),
-        responsive: [
-            {
-              breakpoint: 360,
-              settings: {
-                slidesToShow: 2
-              }
-            },
-          ],
-    });
 
-    $('.reviews__slider ul').slick({
-        infinite: false,
-        speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        prevArrow: $('.prev-arrow'),
-        nextArrow: $('.next-arrow'),
-        responsive: [
-            {
-              breakpoint: 360,
-              settings: {
-                slidesToShow: 1
-              }
-            },
-          ],
-    });
+  prevArrow: $('.prev');
+  nextArrow: $('.next');
+
+  prevArrow: $('.prev-arrow');
+  nextArrow: $('.next-arrow');
+
+  $('.suppliers__slider ul').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    responsive: [
+      {
+        breakpoint: 361,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+    ],
+  });
+
+  $('.reviews__slider ul').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.prev-arrow'),
+    nextArrow: $('.next-arrow'),
+    responsive: [
+      {
+        breakpoint: 361,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+    ],
+  });
+
+  $('.close__burger').on('click',function () {
+    $('.burger__menu').removeClass('active');
+})
+
 
 });
+const burgerMenu = document.querySelector('.media__menu');
+const headerMenu = document.querySelector('.burger__menu');
+
+burgerMenu.addEventListener('click', function () {
+  headerMenu.classList.toggle('active');
+})
